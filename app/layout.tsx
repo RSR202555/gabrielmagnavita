fa4d9c56d14f43a4b8d324e85d671ea12575051e
RSRDEV <rianflamengo8@gmail.com>
2026-04-15 13:58:33 -0300

    Adiciona configuração do Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,6 +52,18 @@ export default function RootLayout({
   return (
     <html lang="pt-BR" className="scroll-smooth">
       <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-NLB533C94W"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-NLB533C94W');
+            `
+          }}
+        />
         <link
           rel="preconnect"
           href="https://fonts.googleapis.com"
